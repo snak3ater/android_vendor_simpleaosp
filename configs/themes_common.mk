e836053002e8e0fdd15bd859bbda82637434f53f
snak3ater <saeedyasir91@gmail.com>
2014-06-30 18:57:48 +0500

    build new theme chooser

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,9 +1,9 @@
 # T-Mobile theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+	ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+	vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+	vendor/simpleaosp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
 
