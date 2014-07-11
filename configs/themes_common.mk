829a53162f6e991a11af27957d79da836cd82d05
MrBaNkS <banksmi09@gmail.com>
2014-07-11 05:48:58 -0500

    This was bothering me

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,9 +1,9 @@
-# T-Mobile theme engine
+# CyanogenMod theme engine
 PRODUCT_PACKAGES += \
-       ThemeChooser \
-	ThemesProvider
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-	vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-	vendor/simpleaosp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/simpleaosp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
 
