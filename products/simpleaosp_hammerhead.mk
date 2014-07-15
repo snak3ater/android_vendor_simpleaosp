004801b6b7bb86102d1b2bce6a5b574d485aa98d
MrBaNkS <banksmi09@gmail.com>
2014-07-15 18:33:48 -0500

    Update hammerhead fingerprint

--- products/simpleaosp_hammerhead.mk
+++ products/simpleaosp_hammerhead.mk
@@ -12,7 +12,7 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.4/KTU84P/1227136:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.4 KTU84P 1227136 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.4/KTU84Q/1253334:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.4 KTU84Q 1253334 release-keys"
 
 # Proprietary libmmcamera_interface.so because of green bar bug
 PRODUCT_COPY_FILES += \
