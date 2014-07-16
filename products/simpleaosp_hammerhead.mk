89b09edb3ff5a99518dfbef0bdac3f3cb40bad65
MrBaNkS <banksmi09@gmail.com>
2014-07-15 20:11:29 -0500

    Move green line camera fix to vendor/lge for hh
    Revert "Green line camera fix"
    
    This reverts commit bca943821c4f3f16eddb3c22816de6b86f4127cb.

--- products/simpleaosp_hammerhead.mk
+++ products/simpleaosp_hammerhead.mk
@@ -13,7 +13,3 @@ PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.4/KTU84Q/1253334:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.4 KTU84Q 1253334 release-keys"
-
-# Proprietary libmmcamera_interface.so because of green bar bug
-PRODUCT_COPY_FILES += \
-    vendor/simpleaosp/prebuilts/common/system/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
