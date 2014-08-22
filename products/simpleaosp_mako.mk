687aadd495f7a08408ec5e4d4a942c6c2ea3cc69
lichti1901 <lichti190185@gmail.com>
2014-08-22 14:56:57 +0200

    Update to new adreno drivers (08.08.2014)
    Conflicts:
    	products/rascarlo_hammerhead.mk

--- products/simpleaosp_mako.mk
+++ products/simpleaosp_mako.mk
@@ -13,3 +13,25 @@ PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4.4/KTU84Q/1253334:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.4 KTU84Q 1253334 release-keys"
+
+# new adreno drivers
+PRODUCT_COPY_FILES += \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/firmware/a330_pfp.fw:system/vendor/firmware/a330_pfp.fw \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/firmware/a330_pm4.fw:system/vendor/firmware/a330_pm4.fw \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
+    vendor/simpleaosp/proprietary/mako/system/NewAdrenoDrivers/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so 
